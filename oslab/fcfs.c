17bf59ab8d72bdff4fcf6e971b60ff02099b5e6f
204g1a0530 <94513740+204g1a0530@users.noreply.github.com>
2022-09-06 11:28:24 +0530

    S.Eswari Created fcfs.c

---
+++ oslab/fcfs.c
@@ -0,0 +1,7 @@
+ 
+/* Name Of the Candidate: S.Eswari
+   Roll No: 204G1A0530
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022
+*/
