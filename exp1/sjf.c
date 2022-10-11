c99cd3ffe8be0d12b726466e4cdd6c7dad1e2124
204g1a0530 <94513740+204g1a0530@users.noreply.github.com>
2022-10-11 11:22:23 +0530

    S Eswari Created sjf.c

---
+++ exp1/sjf.c
@@ -0,0 +1,5 @@
+Name Of the Candidate: S.ESwari
+   Roll No: 204G1A0530
+   Title of the Experiment: CPU Scheduling Using SJF Algorithm
+   Date of Creation: 11-10-2022
+   Date of Execution: 11-10-2022
