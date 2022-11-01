7d4126ab5cbb598df2b0281e7afeb41a635c4f97
204g1a0530 <94513740+204g1a0530@users.noreply.github.com>
2022-11-01 12:54:58 +0530

    S Eswari commited optimal.c

---
+++ page_replacement/optimal.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: 
+   Roll No: 204G1A0530
+   Title of the Experiment: page replacement algorithm using optimal
+   Date of Creation: 1-10-2022
+   Date of Execution: 1-10-2022
+*/
