be7f2104307a7df0dc1f12c4ae55e385b7d0af26
204g1a0530 <94513740+204g1a0530@users.noreply.github.com>
2022-11-30 21:57:00 +0530

    S Eswari created sld.c

---
+++ Exp7/sld.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: S.Eswari
+   Roll No: 204G1A0530
+   Title of the Experiment: Single Level Directory program
+   Date of Creation: 22-11-2022
+   Date of Execution: 22-11-2022
+*/
